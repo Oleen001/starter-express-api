4eee03a211631d64b20f93cb3c9ce4452eba3324
Oleen001 <kuptipong.park@gmail.com>
2024-01-15 23:46:39 +0700

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const logger = require('./middleware/logger');
 
+const cors = require('cors');
+
 const app = express();
 
 
@@ -11,6 +13,18 @@ app.use(express.urlencoded({extended: false}));
 app.use('/api/users', require('./routes/api/users'))
 
 // Init Middleware
+// app.use((req,res, next)=>{
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
+
+// app.use(cors({
+//     origin:'https://oleen-activity.netlify.app'
+// }));
+
+app.use(cors());
+
 app.use(logger);
 
 const PORT = process.env.PORT || 8000;
