bd597a9a37c709af2e00e12cb1cf9ac20e066331
Oleen001 <kuptipong.park@gmail.com>
2024-01-16 00:07:29 +0700

    change to no cors lib

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const logger = require('./middleware/logger');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
@@ -13,17 +13,17 @@ app.use(express.urlencoded({extended: false}));
 app.use('/api/users', require('./routes/api/users'))
 
 // Init Middleware
-// app.use((req,res, next)=>{
-//     res.header('Access-Control-Allow-Origin','*');
-//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use((req,res, next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // app.use(cors({
 //     origin:'https://oleen-activity.netlify.app'
 // }));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(logger);
 
